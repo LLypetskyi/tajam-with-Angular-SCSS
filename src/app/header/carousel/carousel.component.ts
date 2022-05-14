8f3ab89c9ff63f1fc7af38a19f6c0c5c043d7cd8
unknown <liubomyr.lypetskyi@gmail.com>
2022-05-14 19:21:15 +0300

    add some scss to header component

--- src/app/header/carousel/carousel.component.ts
+++ src/app/header/carousel/carousel.component.ts
@@ -9,18 +9,23 @@ export class CarouselComponent implements OnInit {
 
   carousel = [
     {
-      title: "Slide 1 Title",
-      description: "Slide 1 Description",
+      title: "We Are Awesome Creative Agency",
+      description: "This is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit.",
       learnMoreUrl: "https//:some.url",
     },
     {
-      title: "Slide 2 Title",
-      description: "Slide 2 Description",
+      title: "2 We Are Awesome Creative",
+      description: "2 Lorem ipsum dolor sit amet consectetur adipisicing elit.Modi debitis dolor, nesciunt ipsa eveniet suscipit natus molestias et excepturi doloremque, magni magnam in. Sint quaerat aspernatur impedit libero minus natus",
       learnMoreUrl: "https//:some.url",
     },
     {
-      title: "Slide 3 Title",
-      description: "Slide 3 Description",
+      title: "3 We Are Awesome",
+      description: " Lorem ipsum dolor sit amet consectetur adipisicing elit.Nemo, ad beatae corporis at fuga, fugiat aliquid nihil magni, iste autem qui ducimus quod ut aliquam voluptate nisi.Ab, eius sunt!",
+      learnMoreUrl: "https//:some.url",
+    },
+    {
+      title: "4 We Are",
+      description: " Lorem ipsum dolor sit amet consectetur adipisicing elit.Nemo, ad beatae corporis at fuga, fugiat aliquid nihil magn aliquam voluptate nisi.Ab, eius sunt!",
       learnMoreUrl: "https//:some.url",
     }
   ]
