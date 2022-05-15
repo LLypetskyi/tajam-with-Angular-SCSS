13e49bb0077f2c87f4f463cab3e2ca6d05304e15
unknown <liubomyr.lypetskyi@gmail.com>
2022-05-15 20:52:34 +0300

    add image-block to header component

--- src/app/header/carousel/carousel.component.ts
+++ src/app/header/carousel/carousel.component.ts
@@ -11,22 +11,25 @@ export class CarouselComponent implements OnInit {
     {
       title: "We Are Awesome Creative Agency",
       description: "This is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit.",
-      // image: "../../../assets/images/crabs.jpg",
+      image: "../../../assets/images/forest1.jpg",
       learnMoreUrl: "https//:some.url-1 block"
     },
     {
       title: "2 We Are Awesome Creative",
       description: "2 Lorem ipsum dolor sit amet consectetur adipisicing elit.Modi debitis dolor, nesciunt ipsa eveniet suscipit natus molestias et excepturi doloremque, magni magnam in. Sint quaerat aspernatur impedit libero minus natus",
+      image: "../../../assets/images/forest2.jpg",
       learnMoreUrl: "https//:some.url-2 block",
     },
     {
       title: "3 We Are Awesome",
       description: " Lorem ipsum dolor sit amet consectetur adipisicing elit.Nemo, ad beatae corporis at fuga, fugiat aliquid nihil magni, iste autem qui ducimus quod ut aliquam voluptate nisi.Ab, eius sunt!",
+      image: "../../../assets/images/forest3.jpg",
       learnMoreUrl: "https//:some.url-3 block",
     },
     {
       title: "4 We Are",
       description: " Lorem ipsum dolor sit amet consectetur adipisicing elit.Nemo, ad beatae corporis at fuga, fugiat aliquid nihil magn aliquam voluptate nisi.Ab, eius sunt!",
+      image: "../../../assets/images/forest4.jpg",
       learnMoreUrl: "https//:some.url-4 block",
     }
   ]
@@ -41,6 +44,7 @@ export class CarouselComponent implements OnInit {
   redirectTo(url: string) {
     this.selectUrl = url;
     console.log(this.selectUrl)
+    this.selectUrl = "";
   }
 
   constructor() { }
